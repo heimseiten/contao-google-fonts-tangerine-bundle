64b08bcad60462f7a54a332fa6bdbe95b52b3ed4
Niels <info@heimseiten.de>
2022-09-06 20:18:05 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,3 @@
 <?php
 
-if (TL_MODE == 'FE') {
-    $GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontstangerine/css/contao-google-fonts-tangerine-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
-}
+$GLOBALS['TL_HEAD'][] = '<link rel="preload" href="/bundles/heimseitencontaogooglefontstangerine/css/contao-google-fonts-tangerine-bundle.css" as="style" onload="this.onload=null; this.rel=\'stylesheet\';">';
